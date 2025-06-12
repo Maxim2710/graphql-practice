bf23078b32a5aee1f7a299082281ad3edc24c958
PrimakMV <pm2710@mail.ru>
2025-06-12 22:54:36 +0300

    add new get method for get book by id

--- src/main/kotlin/com/graphqlpractice/controller/BookController.kt
+++ src/main/kotlin/com/graphqlpractice/controller/BookController.kt
@@ -2,8 +2,10 @@ package com.graphqlpractice.controller
 
 import com.graphqlpractice.model.Book
 import com.graphqlpractice.repository.BookRepository
+import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import java.util.Optional
 
 @Controller
 class BookController(
@@ -13,4 +15,9 @@ class BookController(
     @QueryMapping
     fun books(): List<Book> =
         bookRepository.getAllBooks()
+
+    @QueryMapping
+    fun bookById(@Argument bookId: Int): Book? =
+        bookRepository.getBookById(bookId)
+
 }
--- src/main/kotlin/com/graphqlpractice/repository/BookRepository.kt
+++ src/main/kotlin/com/graphqlpractice/repository/BookRepository.kt
@@ -2,14 +2,21 @@ package com.graphqlpractice.repository
 
 import com.graphqlpractice.model.Book
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 class BookRepository {
-    fun getAllBooks(): List<Book> =
+    final fun getAllBooks(): List<Book> =
         listOf(
             Book(id = 1, name = "SomeBook", pageCount = 604),
             Book(id = 2, name = "Harry Potter", pageCount = 700),
             Book(id = 3, name = "Foobar", pageCount = 100),
             Book(id = 4, name = "Spring Boot", pageCount = 344)
         )
+
+    fun getBookById(id: Int): Book? =
+        getAllBooks().stream()
+            .filter { it.id == id }
+            .findFirst()
+            .orElse(null)
 }
